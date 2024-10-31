0d056805db69980217b65a0390d233a037df10f0
KillianDbr <debruyne.killian.snt@gmail.com>
2024-10-31 15:06:54 +0100

    Ajout de getVoisins et casesAdjacentesVides

--- HiveGame/Insecte.cpp
+++ HiveGame/Insecte.cpp
@@ -1,2 +1,34 @@
 #include "Insecte.h"
 
+
+std::vector<Hexagon> getVoisins(Hexagon coords)const;{
+        std::vector voisins;
+        voisins.push_back(Hexagon(coords.q+1, coords.r));
+        voisins.push_back(Hexagon(coords.q, coords.r+1));
+        voisins.push_back(Hexagon(coords.q-1, coords.r));
+        voisins.push_back(Hexagon(coords.q, coords.r-1));
+        voisins.push_back(Hexagon(coords.q+1, coords.r-1));
+        voisins.push_back(Hexagon(coords.q-1, coords.r+1));
+        return voisins;
+    }
+
+std::vector<Hexagon> casesAdjacentesVides(Hexagon coords, Plateau p){
+    std::vector<Hexagon> vides; //On déclare la liste des hexagons adjacents vides, pour le moment elle ne contient rien
+
+    std::vector<Hexagon> voisins = getVoisins(coords); //On déclare la liste des voisins de l'hexagon cible
+    for(size_t i; i<6; i++){
+        for(size_t n; n<p.plateauMap.size(); n++){
+            auto it = p.plateauMap.begin();
+            std::advance(it, n); // On parcours chaque voisins et vérifions si ses coordonnées sont déjà enregistrées dans le plateau,
+                                 //si c'est le cas cela signifie qu'il y a un insecte sur cette case
+            if(it != p.plateauMap.end()){
+                const Hexagon &hex = it->first;
+                if(voisins.at(i).getQ() != hex.getQ() || voisins.at(i).getR()!=hex.getR()){
+                    vides.push_back(voisins.at(i));
+                }
+            }
+        }
+    }
+    return vides;
+}
+
--- HiveGame/Insecte.h
+++ HiveGame/Insecte.h
@@ -2,10 +2,14 @@
 #ifndef INSECTE_H
 #define INSECTE_H
 #include "Hexagon.h"
+#include "Plateau.h"
 #include <string>
 #include <vector>
 
+
 class Joueur; // Déclaration anticipée de Joueur pour éviter l'inclusion circulaire
+
+
 class Insecte
 {
 private:
@@ -18,7 +22,6 @@ public:
     Insecte(std::string nom, Hexagon coords) : nom(nom), coords(coords) {}
     Hexagon getCoords() const { return coords; }
     std::string getNom() const { return nom; }
-    virtual std::vector<Hexagon> deplacementsPossibles() const = 0; // La méthode est virtuelle pour que chaque enfant ait la sienne
     void setDessus(Insecte *insecte){
         dessus = insecte;
     }
@@ -30,70 +33,57 @@ public:
     }
 };
 
+
 class ReineAbeille : public Insecte {
 public:
     ReineAbeille(Hexagon coords) : Insecte("Reine", coords) {}
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
 
 class Fourmi : public Insecte {
 public:
     Fourmi(Hexagon coords) : Insecte("Fourmi", coords) {}
-
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Logique pour les déplacements possibles de la fourmi
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
 
 class Sauterelle : public Insecte {
 public:
     Sauterelle(Hexagon coords) : Insecte("Sauterelle", coords) {}
-
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
 
 class Coccinelle : public Insecte {
 public:
     Coccinelle(Hexagon coords) : Insecte("Coccinelle", coords) {}
-
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
+
 class Scarabee : public Insecte {
 public:
     Scarabee(Hexagon coords) : Insecte("Scarabee", coords) {}
-
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
+
 class Araignee : public Insecte {
 public:
     Araignee(Hexagon coords) : Insecte("Araignée", coords) {}
-
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
 
 class Moustique : public Insecte {
 public:
     Moustique(Hexagon coords) : Insecte("Moustique", coords) {}
 
-    std::vector<Hexagon> deplacementsPossibles() const {
-        // Retourne une liste de coordonnées hexagonales
-        return {};
-    }
+    std::vector<Hexagon> deplacementsPossibles() const;
 };
+
+std::vector<Hexagon> getVoisins(Hexagon coords)const;
+std::vector<Hexagon> casesAdjacentesVides(Hexagon coords, Plateau p)const;
+
 #endif // INSECTE_H
